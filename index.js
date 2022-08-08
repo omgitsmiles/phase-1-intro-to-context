4db5aaa8599aafb74afdc3599f9b26beeb8d993f
+ <+>
2022-08-08 10:37:08 -0400

    commit tree 4627f88fb121a51dcf8e20da766ffc1088715afb

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(employee => createEmployeeRecord(employee))
+}
+
+const createTimeInEvent = function (employeeRec, dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    // const dateArr =  dateStamp.split(' ')
+    // const hour = dateArr[0]
+    // const date = dateArr[1]
+    const eventObj = {
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(eventObj)
+    return this
+}
+
+function createTimeOutEvent(employeeRec, dateStamp) {
+    const dateArr = dateStamp.split(' ')
+    const hour = dateArr[0]
+    const date = dateArr[1]
+    const eventObj = {
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(eventObj)
+    return this
+}
+
+function hoursWorkedOnDate(record, date){
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === date)
+    const outEvent = this.timeOutEvents.find(oEvent => oEvent.date === date)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(object, date){
+
+}
+
+function allWagesFor(object){
+
+}
+
+function calculatePayroll(array){
+
+}
